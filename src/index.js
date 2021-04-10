2900b47c3a81f4631545802a347b5b3cc4a3a56d
vladimirkostenko <mr.selcet@gmail.com>
2021-04-10 13:00:02 +0300

    rp-19: gh-pages for serving

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import emailjs from 'emailjs-com';
-
-// import required modules
-// import './js/email-js';
-import './js/menu-toggler';
-
-// init email js with user id
-emailjs.init('user_ktvRisjZ0eN3EVUq75tK2');
